01e85ea1e9e8b00c95b5d7a2302a0934f94b19a3
Mikhail Volf <misha.volf93@gmail.com>
2022-12-09 12:19:48 +0300

    entry point added

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import time
 from vk_profile import Profile
 from ya_uploader import YaUploader
 
-# скачивания информации о пользователе на ПК
-start_time = time.time()
-Person = Profile(input('Введите ID пользователя: '))
-Person.save_photos()
 
-# Загрузка в Яндекс диск
-uploader = YaUploader()
-result = uploader.upload_files(Person.path_photos)
-# print(result)
-print(f'Время выполнения: {time.time() - start_time}')
+def main():
+    # скачивания информации о пользователе на ПК
+    start_time = time.time()
+    person = Profile(input('Введите ID пользователя: '))
+    person.save_photos()
+
+    # Загрузка в Яндекс диск
+    uploader = YaUploader()
+    result = uploader.upload_files(person.path_photos)
+    # print(result)
+    print(f'Время выполнения: {time.time() - start_time}')
+
+
+if __name__ == '__main__':
+    main()
